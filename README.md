bede3a25f505e4aa2794165c3700c84527bd2a52
ferrophile <hwpang@ust.hk>
2018-07-05 21:20:57 +0200

    bonus requirements done

--- README.md
+++ README.md
@@ -3,11 +3,20 @@ IN2346 2018 SoSe Exercises
 *Hong Wing PANG*
 
 * Exercise 0
-* Exercise 1 (Deadline: 23/5)
+* Exercise 1 (23rd May)
     * 1.1 Softmax Classification (with Cross-Entropy Loss)
     * 1.2 Implementing a Neural Network
     * 1.3 Image Features
-* Exercise 2 (Deadline: 6/6)
-    * 2.1 Softmax Classification (with Cross-Entropy Loss)
-    * 2.2 Batch Normalization
-    * 2.3 Dropout (optional exercise)
+* Exercise 2 (6th Jun)
+    * 2.1 Fully-connected Neural Nets
+    * *2.2 Batch Normalization*
+    * *2.3 Dropout*
+* Exercise 3 (22nd Jun)
+    * *3.0.1 CNN Layers*
+    * *3.0.2 Spatial Batch Normalization*
+    * *3.0.3 PyTorch Introduction*
+    * 3.1 Image Classification with CNNs
+* Exercise 4 (4th Jul)
+    * 4.0 Semantic Segmentation
+    * 4.1 Facial Keypoint Detection
+    * *4.2 Facial Filters*
--- exercise_4/exercise_code/segmentation_nn.py
+++ exercise_4/exercise_code/segmentation_nn.py
@@ -2,6 +2,10 @@
 import torch
 import torch.nn as nn
 
+# Model implementation modified from
+# https://github.com/meetshah1995/pytorch-semseg
+import torch.nn.functional as F
+import torchvision.models as models
 
 class SegmentationNN(nn.Module):
 
@@ -12,7 +16,86 @@ class SegmentationNN(nn.Module):
         #                             YOUR CODE                                #
         ########################################################################
 
-        pass
+        self.n_classes = num_classes
+
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=100),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),)
+
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),)
+
+        self.conv_block3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),)
+
+        self.conv_block4 = nn.Sequential(
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),)
+
+        self.conv_block5 = nn.Sequential(
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, stride=2, ceil_mode=True),)
+
+        self.classifier = nn.Sequential(
+            nn.Conv2d(512, 4096, 7),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(),
+            nn.Conv2d(4096, 4096, 1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(),
+            nn.Conv2d(4096, self.n_classes, 1),)
+
+        self.score_pool4 = nn.Conv2d(512, self.n_classes, 1)
+        self.score_pool3 = nn.Conv2d(256, self.n_classes, 1)
+
+        # Set weights from pre-trained Pytorch VGG-16 model
+
+        vgg16 = models.vgg16(pretrained=True)
+        blocks = [self.conv_block1,
+                  self.conv_block2,
+                  self.conv_block3,
+                  self.conv_block4,
+                  self.conv_block5]
+
+        ranges = [[0, 4], [5, 9], [10, 16], [17, 23], [24, 29]]
+        features = list(vgg16.features.children())
+
+        for idx, conv_block in enumerate(blocks):
+            for l1, l2 in zip(features[ranges[idx][0]:ranges[idx][1]], conv_block):
+                if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
+                    assert l1.weight.size() == l2.weight.size()
+                    assert l1.bias.size() == l2.bias.size()
+                    l2.weight.data = l1.weight.data
+                    l2.bias.data = l1.bias.data
+
+        for i1, i2 in zip([0, 3], [0, 3]):
+            l1 = vgg16.classifier[i1]
+            l2 = self.classifier[i2]
+            l2.weight.data = l1.weight.data.view(l2.weight.size())
+            l2.bias.data = l1.bias.data.view(l2.bias.size())
 
         ########################################################################
         #                             END OF YOUR CODE                         #
@@ -30,7 +113,21 @@ class SegmentationNN(nn.Module):
         #                             YOUR CODE                                #
         ########################################################################
 
-        pass
+        conv1 = self.conv_block1(x)
+        conv2 = self.conv_block2(conv1)
+        conv3 = self.conv_block3(conv2)
+        conv4 = self.conv_block4(conv3)
+        conv5 = self.conv_block5(conv4)
+
+        score = self.classifier(conv5)
+        score_pool4 = self.score_pool4(conv4)
+        score_pool3 = self.score_pool3(conv3)
+
+        score = F.upsample_bilinear(score, score_pool4.size()[2:])
+        score += score_pool4
+        score = F.upsample_bilinear(score, score_pool3.size()[2:])
+        score += score_pool3
+        x = F.upsample_bilinear(score, x.size()[2:])
 
         ########################################################################
         #                             END OF YOUR CODE                         #
